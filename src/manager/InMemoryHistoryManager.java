56dfaf7aaf97d1cb98418b1c90db2d3ecaec6196
ds <d.a.shevelev@yandex.ru>
2023-05-10 16:14:46 +0300

    fix: поменял tasks на history, убрал конструктор с параметрами, длину истории сделал константой

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -3,38 +3,27 @@ package manager;
 import entities.Task;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final int MAXIMUM_HISTORY_LENGTH;
-    private final List<Task> tasks;
+    private final static int MAXIMUM_HISTORY_LENGTH = 10;
+    private final List<Task> history;
 
     public InMemoryHistoryManager() {
-        this.MAXIMUM_HISTORY_LENGTH = 10;
-        this.tasks = new ArrayList<>();
-    }
-
-    public InMemoryHistoryManager(int MAXIMUM_HISTORY_LENGTH) {
-        if (MAXIMUM_HISTORY_LENGTH > -1) {
-            this.MAXIMUM_HISTORY_LENGTH = MAXIMUM_HISTORY_LENGTH;
-        } else {
-            this.MAXIMUM_HISTORY_LENGTH = 10;
-        }
-        this.tasks = new ArrayList<>();
+        this.history = new ArrayList<>();
     }
 
     @Override
     public void add(Task task) {
-        tasks.add(task);
-        if (tasks.size() > MAXIMUM_HISTORY_LENGTH) {
-            tasks.remove(0);
+        history.add(task);
+        if (history.size() > MAXIMUM_HISTORY_LENGTH) {
+            history.remove(0);
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return tasks;
+        return history;
     }
 }
